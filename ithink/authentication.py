786d0a764d7c645a89afa0a6eb47572e4874216c
anton <anton.tymoshchuk.personal@gmail.com>
2020-09-19 21:20:57 +0300

    Created basic blog functions and new authentication functions

--- ithink/authentication.py
+++ ithink/authentication.py
@@ -5,7 +5,7 @@ from flask import (Blueprint, session, g, url_for, request, render_template,
 from werkzeug.utils import redirect
 
 from ithink.database import get_database
-from ithink.security.cryptography import ascii_encrypt
+from ithink.security.cryptography import ascii_encrypt, ascii_decrypt
 
 authentication = Blueprint('authentication', __name__,
                            url_prefix='/authentication')
@@ -109,6 +109,65 @@ def logout():
     return redirect(url_for('blog.index'))
 
 
+@authentication.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'GET':
+        database = get_database()
+        user_id = session.get('user_id')
+        if user_id is not None:
+            test_user = database.execute('SELECT * FROM Users '
+                                         'WHERE Id=?', (user_id,)).fetchone()
+            if test_user is not None:
+                g.username = ascii_decrypt(test_user['Username'])
+                g.email = ascii_decrypt(test_user['Email'])
+                g.posts = database.execute('SELECT * FROM Posts '
+                                           'WHERE Id=?', (user_id,)).fetchall()
+                return render_template('authentication/edit.html')
+        return redirect('blog.index')
+    elif request.method == 'POST':
+        error = None
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+        if request.form['username'] is None:
+            error = 'You must enter your username!'
+        elif request.form['email'] is None:
+            error = 'You must enter your email!'
+        elif request.form['password'] is None:
+            error = 'You must enter your password!'
+        elif len(request.form['username']) < 2:
+            error = 'Your username is too short, it must be at least 2 ' \
+                    'symbols long!'
+        elif len(request.form['password']) < 8:
+            error = 'Your password is too short, it must be at least 8 ' \
+                    'symbols long!'
+        elif len(request.form['username']) > 256:
+            error = 'Your username is to long, it must be shorter then 256 ' \
+                    'symbols!'
+        elif len(request.form['password']) > 256:
+            error = 'Your password is to long, it must be shorter then 256 ' \
+                    'symbols!'
+        elif re.search(regex, request.form['email']) is None:
+            error = 'Email is invalid, please check it out again!'
+        if error is None:
+            username = ascii_encrypt(request.form['username'])
+            email = ascii_encrypt(request.form['email'])
+            password = ascii_encrypt(request.form['password'])
+            database = get_database()
+            test_user = database.execute('SELECT * FROM Users '
+                                         'WHERE Email=?', (email,)).fetchone()
+            if test_user is not None:
+                database.execute('UPDATE Users SET '
+                                 'Username=?, Email=?, Password=? '
+                                 'WHERE Id=?', (username, email,
+                                                password, test_user['Id'],))
+                database.commit()
+                return redirect(url_for('blog.index'))
+            else:
+                error = 'Sorry, but this user already exists!'
+        flash(error)
+        return render_template('authentication/edit.html')
+
+
 @authentication.route('/delete', methods=['GET', 'POST'])
 def delete():
     if request.method == 'GET':
@@ -125,12 +184,15 @@ def delete():
             if test_user is not None:
                 database.execute('DELETE FROM Users '
                                  'WHERE Id=?', (user_id,))
+                database.execute('DELETE FROM Posts '
+                                 'Where Author=?', (user_id,))
                 database.commit()
             else:
-                error = 'Incorrect password!'
+                error = 'Could not find such user!'
         else:
-            error = 'Could not find such user!'
-        if error is not None:
+            error = 'Some cache error!'
+        if error is None:
+            return redirect(url_for('blog.index'))
+        else:
             flash(error)
             return render_template('authentication/delete.html')
-        return redirect(url_for('blog.index'))
--- ithink/blog.py
+++ ithink/blog.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, session, g, render_template
+from flask import Blueprint, session, g, render_template, request, url_for, \
+    flash
+from werkzeug.utils import redirect
 
 from ithink.database import get_database
-from ithink.security.cryptography import ascii_decrypt
+from ithink.security.cryptography import ascii_encrypt, ascii_decrypt
 
 blog = Blueprint('blog', __name__)
 
@@ -17,6 +19,122 @@ def index():
             g.username = ascii_decrypt(test_user['Username'])
             g.posts = database.execute('SELECT * FROM Posts '
                                        'WHERE Id=?', (user_id,)).fetchall()
-    g.posts = database.execute('SELECT * FROM Posts '
-                               'ORDER BY Created DESC').fetchall()
+    enc_posts = database.execute('SELECT * FROM Posts '
+                                 'ORDER BY Created DESC').fetchall()
+    g.posts = []
+    for enc_post in enc_posts:
+        author = database.execute('SELECT * FROM Users WHERE Id=?',
+                                  (enc_post['Author'],)).fetchone()
+        author = ascii_decrypt(author['Username'])
+        post = {'Id': enc_post['Id'],
+                'Theme': ascii_decrypt(enc_post['Theme']),
+                'Content': ascii_decrypt(enc_post['Content']),
+                'Created': enc_post['Created'],
+                'Author': author}
+        g.posts.append(post)
     return render_template('blog/blog.html')
+
+
+@blog.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('blog/create.html')
+    elif request.method == 'POST':
+        error = None
+        if request.form['theme'] is None:
+            error = 'You must enter a theme of your post!'
+        elif request.form['content'] is None:
+            error = 'You must enter some text of your post!'
+        elif len(request.form['theme']) < 2:
+            error = 'Your theme is too short, it must be at least 2 ' \
+                    'symbols long!'
+        elif len(request.form['theme']) > 256:
+            error = 'Your theme is to long, it must be shorter then 256 ' \
+                    'symbols!'
+        if error is None:
+            user_id = session.get('user_id')
+            if user_id is not None:
+                database = get_database()
+                test_user = database.execute('SELECT * FROM Users '
+                                             'WHERE Id=?',
+                                             (user_id,)).fetchone()
+                if test_user is not None:
+                    theme = ascii_encrypt(request.form['theme'])
+                    content = ascii_encrypt(request.form['content'])
+                    database = get_database()
+                    database.execute('INSERT INTO Posts'
+                                     '(Author, Theme, Content) '
+                                     'VALUES (?, ?, ?)',
+                                     (session.get('user_id'),
+                                      theme, content,))
+                    database.commit()
+                    return redirect(url_for('blog.index'))
+                else:
+                    error = 'Could not find such user!'
+            else:
+                error = 'Some cache error!'
+        flash(error)
+        return render_template('blog/create.html')
+
+
+@blog.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit(post_id):
+    if request.method == 'GET':
+        database = get_database()
+        test_post = database.execute('SELECT * FROM Posts WHERE Id=?',
+                                     (post_id,)).fetchone()
+        if test_post is not None:
+            g.post = {'Theme': ascii_decrypt(test_post['Theme']),
+                      'Content': ascii_decrypt(test_post['Content'])}
+            return render_template('blog/edit.html')
+        return redirect('blog.index')
+    elif request.method == 'POST':
+        error = None
+        if request.form['theme'] is None:
+            error = 'You must enter a theme of your post!'
+        elif request.form['content'] is None:
+            error = 'You must enter some text of your post!'
+        elif len(request.form['theme']) < 2:
+            error = 'Your theme is too short, it must be at least 2 ' \
+                    'symbols long!'
+        elif len(request.form['theme']) > 256:
+            error = 'Your theme is to long, it must be shorter then 256 ' \
+                    'symbols!'
+        if error is None:
+            theme = ascii_encrypt(request.form['theme'])
+            content = ascii_encrypt(request.form['content'])
+            database = get_database()
+            database.execute('UPDATE Posts SET Theme=?, Content=? '
+                             'WHERE Id=?', (theme, content, post_id,))
+            database.commit()
+            return redirect(url_for('blog.index'))
+        flash(error)
+        return render_template('blog/edit.html')
+
+
+@blog.route('/delete/<int:post_id>', methods=['GET', 'POST'])
+def delete(post_id):
+    if request.method == 'GET':
+        return render_template('blog/delete.html')
+    elif request.method == 'POST':
+        error = None
+        user_id = session.get('user_id')
+        if user_id is not None:
+            password = ascii_encrypt(request.form['password'])
+            database = get_database()
+            test_user = database.execute('SELECT * FROM Users '
+                                         'WHERE Id=? and Password=?',
+                                         (user_id, password)).fetchone()
+            if test_user is not None:
+                database.execute('DELETE FROM Posts '
+                                 'WHERE Id=?', (post_id,))
+                database.commit()
+            else:
+                error = 'Could not find such user!'
+        else:
+            error = 'Some cache error!'
+        if error is None:
+            return redirect(url_for('blog.index'))
+        else:
+            flash(error)
+            return render_template('blog/delete.html')
